2227744770b3935682b20a4421345bc59c137240
ashjorth <andreas.deer@gmail.com>
2021-05-02 21:13:34 +0200

    Add resize function and parameter optimalization

--- cartoonify.py
+++ cartoonify.py
@@ -1,21 +1,39 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
+filename = 'IMG_1288.jpg'
 
+def resizeImage(image):
+    
+    scale_ratio = 0.3
+    width = int(image.shape[1] * scale_ratio)
+    height = int(image.shape[0] * scale_ratio)
+    new_dimensions = (width, height)
+    resized = cv2.resize(image, new_dimensions, interpolation = cv2.INTER_AREA)
 
+    return resized
 
-input_path = 'Slottet_i_Oslo.jpg'
-output_path =  None
-
-# Step 1: Read image from path
-
-# Step 2: Find contours
+# Find contours
 def findCountours(image):
-    pass
     
+    contoured_image = image
     
-# Step 3: Color quantization 
+    gray = cv2.cvtColor(contoured_image, cv2.COLOR_BGR2GRAY) 
+    
+    edged = cv2.Canny(gray, 30, 100)
+    
+    contours, hierarchy = cv2.findContours(edged, 
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    cv2.drawContours(contoured_image, contours, contourIdx=-1, color=1, thickness=1)
+    
+    cv2.imshow('Image after countouring', contoured_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    return contoured_image
+    
+# Color quantization 
 def ColorQuantization(image, K=4):
     
     Z = image.reshape((-1, 3))  
@@ -26,24 +44,32 @@ def ColorQuantization(image, K=4):
     
     # Define critera and apply kmeans()
     
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    compactness, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10000, 0.0001)
+    compactness, label, center = cv2.kmeans(Z, K, None, criteria, 1, cv2.KMEANS_RANDOM_CENTERS)
     
     # Convert to uint8 and apply to original image
     center = np.uint8(center)
     res = center[label.flatten()]
     res2 = res.reshape((image.shape))
     
-    cv2.imshow('res2',res2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
     return res2
-# Step n: Combine images
+
 
 if __name__ == "__main__":
     
-    image = cv2.imread(input_path)
+    image = cv2.imread(filename)
+    resized_image = resizeImage(image)
+    coloured = ColorQuantization(resized_image)
+    contoured = findCountours(coloured)
     
-    image2 = ColorQuantization(image)
+    final_image = contoured
+    
+    save_q = input("Save the image? [y]/[n] ")
+    
+    if save_q == "y":
+           
+        cv2.imwrite("cartoonized_"+ filename, final_image)
+        print("Image saved!")
+    
+    
     
